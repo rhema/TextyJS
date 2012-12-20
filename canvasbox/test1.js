fcdf2d4fb480e1afbecdc84687d33d1b9c6c8783
Rhema Linder <rhemalinder@gmail.com>
2012-12-20 10:07:12 -0600

    Imgur upload example.

--- canvasbox/test1.js
+++ canvasbox/test1.js
@@ -143,3 +143,37 @@ function paint_text(text, x, y)
 	var w = context.measureText(text).width;
 	touched.push(new box(x,y,w,30));
 }
+
+
+
+function share(){
+    try {
+        var img = canvas.toDataURL('image/jpeg', 0.9).split(',')[1];
+    } catch(e) {
+        var img = canvas.toDataURL().split(',')[1];
+    }
+    // open the popup in the click handler so it will not be blocked
+    var w = window.open();
+    w.document.write('Uploading...');
+    // upload to imgur using jquery/CORS
+    // https://developer.mozilla.org/En/HTTP_access_control
+    $.ajax({
+        url: 'http://api.imgur.com/2/upload.json',
+        type: 'POST',
+        data: {
+            type: 'base64',
+            // get your key here, quick and fast http://imgur.com/register/api_anon
+            key: 'd0fe9f9c7aed6c55df43f2d5583ba6dd',
+            name: 'neon.jpg',
+            title: 'rhema is testing the',
+            caption: 'and my favorite subtext is',
+            image: img
+        },
+        dataType: 'json'
+    }).success(function(data) {
+        w.location.href = data['upload']['links']['imgur_page'];
+    }).error(function() {
+        alert('Could not reach api.imgur.com. Sorry :(');
+        w.close();
+    });
+}
